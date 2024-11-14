bdd7c52d7ed87f8eb075cd668f8f6ac40f3bc6f2
Giuseppe Giardinetto <giuseppegiardinetto1@gmail.com>
2024-11-14 16:14:04 +0100

    Update main.py
    Added string stemming

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
 import os
+import re
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from nltk import SnowballStemmer
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.snowball import SnowballStemmer
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 import seaborn as sn
 import datasetDownloader as dD
 
+
+nltk.download('stopwords')
+stemmer = SnowballStemmer("english")
+
+def stem(string):
+    string = str(string)
+    stemmed_string = string
+    stemmed_string = stemmed_string.lower()
+    stemmed_string = stemmed_string.split()
+    stemmed_string = [stemmer.stem(word) for word in stemmed_string if word not in stopwords.words('english')]
+    stemmed_string = ' '.join(stemmed_string)
+    return stemmed_string
+
+
+
 path="WELFake_Dataset.csv"
-pd.set_option('display.max_columns', 5)
 
 if not os.path.exists(path):
     print("File does not exist. Downloading")
@@ -34,5 +53,13 @@ print("Dataframe rows: {}".format(df.shape[0]))
 print("\nDataframe's null values:")
 print(df.isnull().sum())
 
+### Plot results
 sn.countplot(x='label', hue='label',legend=False, data = df, palette= 'mako')
 plt.show()
+
+####### Stemming
+df['text'] = df['text'].apply(stem)
+df['title'] = df['title'].apply(stem)
+
+print("\nDataframe's stemmed titles:\n"+df['title'].head())
+print("\nDataframe's stemmed texts:\n"+df['text'].head())
