7fa6128507d776d3e9a6b267ded72f05eea9909a
Giuseppe Giardinetto <giuseppegiardinetto1@gmail.com>
2024-11-14 13:17:22 +0100

    Update main.py
    Data cleaning

--- main.py
+++ main.py
@@ -8,16 +8,31 @@ import seaborn as sn
 import datasetDownloader as dD
 
 path="WELFake_Dataset.csv"
+pd.set_option('display.max_columns', 5)
 
 if not os.path.exists(path):
     print("File does not exist. Downloading")
     dD.download()
+else:
+    print("File already downloaded")
 
 df = pd.read_csv(path)
+
+#######Data cleaning
 print("Dataframe before drop")
-df.head()
-df.drop('Unnamed: 0', axis='columns', inplace=True)
-print("Dataframe after drop")
-df.head()
-sn.barplot(x='label', data = df, palette= 'mako')
+print(df.head())
+df.drop(columns=['Unnamed: 0'], axis=1, inplace=True)
+print("\nDataframe after drop")
+print(df.head())
+
+print("Dataframe rows: {}".format(df.shape[0]))
+print("\nDataframe's null elements:")
+print(df.isnull().sum())
+
+df.dropna(inplace=True)
+print("Dataframe rows: {}".format(df.shape[0]))
+print("\nDataframe's null values:")
+print(df.isnull().sum())
+
+sn.countplot(x='label', hue='label',legend=False, data = df, palette= 'mako')
 plt.show()
